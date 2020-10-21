25716b88f3abe3a8f16dbcc2679b3f0a9944ffca
wangruohui <12756472+wangruohui@users.noreply.github.com>
2020-10-21 15:47:58 +0800

    remove voc2012, use bs=4

--- configs/tutorial/voc07.py
+++ configs/tutorial/voc07.py
@@ -31,18 +31,15 @@ test_pipeline = [
     ),
 ]
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
         type="RepeatDataset",
         times=3,
         dataset=dict(
             type=dataset_type,
-            ann_file=[
-                data_root + "VOC2007/ImageSets/Main/trainval.txt",
-                data_root + "VOC2012/ImageSets/Main/trainval.txt",
-            ],
-            img_prefix=[data_root + "VOC2007/", data_root + "VOC2012/"],
+            ann_file=data_root + "VOC2007/ImageSets/Main/trainval.txt",
+            img_prefix=data_root + "VOC2007/",
             pipeline=train_pipeline,
         ),
     ),
